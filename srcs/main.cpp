e005f70230d18d0c8590ed8fc9c17927567b3c7e
Tommy <tommy@Tommys-MacBook-Pro.local>
2024-05-14 15:38:42 +0400

    password length

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,19 +4,25 @@
 //master socket?
 //listen, max queue pending connecton recommended size(2 arg)
 // 2 channel name search till the end
-
-int	ValidateInput(int argc, std::string stringPort)
+// pass length 8
+int	ValidateInput(int argc, std::string stringPort, std::string password)
 {
 	if (argc != 3)
 	{
-		std::cout << RED << "Wrong number of arguments!!" << RESET << std::endl // avelacnel guynery
+		std::cout << BOLDRED << "Wrong number of arguments!!" << RESET << std::endl // avelacnel guynery
 			<< "Try like: ./irc <port> <password>" << std::endl; // avelacnel guynery
+		exit(EXIT_FAILURE);
+	}
+	if (password.length() < 8)
+	{
+		std::cout << BOLDRED << "[ERROR] Incorrect length of the password" << BOLDYELLOW << std::endl // avelacnel guynery
+		<< "Should have at least 8 characters" << RESET << std::endl; // avelacnel guynery
 		exit(EXIT_FAILURE);
 	}
 	int port = custom_stoi(stringPort);
 	if (port < 0 || port > 65535 || stringPort.find_first_not_of("0123456789") != std::string::npos)
 	{
-		std::cout << RED << "Wrong PORT specified!!! Please input a valid PORT" << YELLOW << std::endl // avelacnel guynery
+		std::cout << BOLDRED << "Wrong PORT specified!!! Please input a valid PORT" << BOLDYELLOW << std::endl // avelacnel guynery
 		<< "It should be a number between 0 and 65535" << RESET << std::endl; // avelacnel guynery
 		exit(EXIT_FAILURE);
 	}
@@ -25,7 +31,7 @@ int	ValidateInput(int argc, std::string stringPort)
 
 int main(int argc, char **argv)
 {
-	Server	server(ValidateInput(argc, argv[1]), argv[2]);
+	Server	server(ValidateInput(argc, argv[1], argv[2]), argv[2]);
 
 	server.Setup();
 
